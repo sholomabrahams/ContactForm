32d582d0e37ff39739ef043892cc159787123a70
sholomabrahams <sholomabrahams@icloud.com>
2020-11-20 14:30:10 -0500

    Removed redundant files

--- Contact.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import reqparse, Resource
-from sqlalchemy import insert
-
-from Database import contact_table, conn
-
-
--- Database.py
+++
@@ -1,2 +0,0 @@
-from sqlalchemy import MetaData, create_engine
-
